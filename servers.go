7f7d833e22e1a16ba5c96269e9541eeca5589b5a
Nicolas Crausaz <n.crausaz99@gmail.com>
2023-01-20 18:31:03 +0100

    Base of P&E

--- servers.go
+++ servers.go
@@ -5,9 +5,14 @@ import (
 	. "SDR-Labo4/src/server"
 	"SDR-Labo4/src/server/algo"
 	"SDR-Labo4/src/utils/log"
+	"flag"
 )
 
 func main() {
+	serverMode := flag.Int("mode", 1, "mode")
+	flag.Parse()
+	log.Logf(log.Info, "Selected mode: %d", *serverMode)
+
 	c, err := config.LoadConfig()
 	if err != nil {
 		log.Logf(log.Fatal, "Error loading config: %s", err)
@@ -24,9 +29,23 @@ func main() {
 		server.Start()
 	}
 
-	for _, server := range servers {
-		wave := algo.NewWave(*server, len(servers))
-		go wave.Run()
+	// Wave mode
+	if *serverMode == 1 {
+		for _, server := range servers {
+			wave := algo.NewWave(*server, len(servers))
+			go wave.Run()
+		}
+	} else if *serverMode == 2 {
+		// Probes & Echoes mode
+		for i, server := range servers {
+			probes := algo.NewProbesAndEchoes(*server)
+			if i != 0 {
+				go probes.StartAsNode()
+			}
+		}
+		// Start the first server as the "root"
+		root := algo.NewProbesAndEchoes(*servers[0])
+		go root.StartAsRoot()
 	}
 
 	select {}
